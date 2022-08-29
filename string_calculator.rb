f8b909b3d5574252832134923aecaebdc610860c
Sushil <sushilt553@gmail.com>
2022-08-29 18:55:07 -0400

    modify add method to handle two chars string

--- string_calculator.rb
+++ string_calculator.rb
@@ -1,6 +1,12 @@
 class StringCalculator
     def add(numbers)
         0 if numbers == ''
-        numbers.to_i
+        nums_arr = numbers.split(",")
+        sum = 0
+
+        nums_arr.each do |num|
+            sum += num.to_i
+        end
+        sum
     end
 end
