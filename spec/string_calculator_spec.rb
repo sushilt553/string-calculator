28a6fc0a18199461134c6b3bd9836a7eb96198f2
Sushil <sushilt553@gmail.com>
2022-08-29 18:06:31 -0400

    add test for non-empty string for add method and fail it

--- spec/string_calculator_spec.rb
+++ spec/string_calculator_spec.rb
@@ -2,10 +2,15 @@ require 'spec_helper'
 require_relative '../string_calculator.rb'
 
 describe StringCalculator do
+
+    let(:string_calculator) { StringCalculator.new }
+
     describe "#add" do
         it "returns 0 when empty string" do
-            string_calculator = StringCalculator.new
             expect(string_calculator.add("")).to eq(0)
+        end
+        it "adds numbers and returns sum total" do
+            expect(string_calculator.add("1")).to eq(1)
         end
     end
 end
