0f10da69d0a4b5923ac7750b9597a9d1a85aa56e
Sushil <sushilt553@gmail.com>
2022-08-29 18:56:51 -0400

    refactor #add method

--- string_calculator.rb
+++ string_calculator.rb
@@ -1,12 +1,5 @@
 class StringCalculator
     def add(numbers)
-        0 if numbers == ''
-        nums_arr = numbers.split(",")
-        sum = 0
-
-        nums_arr.each do |num|
-            sum += num.to_i
-        end
-        sum
+        numbers.split(",").map{|num| num.to_i}.sum
     end
 end
