c807cd71aa136e01ad799ade110a37250825e760
Sushil <sushilt553@gmail.com>
2022-08-29 19:18:39 -0400

    modify add method to handle delimiters in the string

--- string_calculator.rb
+++ string_calculator.rb
@@ -1,11 +1,5 @@
 class StringCalculator
     def add(numbers)
-        numbers.split(",").map do |num| 
-            if num.include?("\n")
-                num.split("\n").map{|n| n.to_i}.sum
-            else
-                num.to_i
-            end
-        end.sum
+        numbers.split("").map{|num| num.to_i}.sum
     end
 end
