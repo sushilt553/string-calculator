e57136a1e6ae7f679f301e1a73229771d7efb3b7
Sushil <sushilt553@gmail.com>
2022-08-29 18:00:15 -0400

    add test for add method with empty string as an argument and fail it

--- spec/string_calculator_spec.rb
+++ spec/string_calculator_spec.rb
@@ -2,5 +2,10 @@ require 'spec_helper'
 require_relative '../string_calculator.rb'
 
 describe StringCalculator do
-
+    describe "#add" do
+        it "returns 0 when empty string" do
+            string_calculator = StringCalculator.new
+            expect(string_calculator.add("")).to eq(0)
+        end
+    end
 end
