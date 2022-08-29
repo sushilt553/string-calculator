692f1d87a0dc8a87a2e587515e30e47a178ebebe
Sushil <sushilt553@gmail.com>
2022-08-29 19:57:58 -0400

    modify add method to raise exception for negative numbers in the string

--- string_calculator.rb
+++ string_calculator.rb
@@ -1,5 +1,6 @@
 class StringCalculator
     def add(numbers)
+        raise "negatives not allowed - #{numbers}" if numbers.include?('-')
         numbers.split("").map{|num| num.to_i}.sum
     end
 end
