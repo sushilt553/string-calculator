910313e917c661dbad1745810542bd5ea7358e95
Sushil <sushilt553@gmail.com>
2022-08-29 20:03:59 -0400

    modify add method to raise exception for multiple negatives in the string

--- string_calculator.rb
+++ string_calculator.rb
@@ -1,6 +1,29 @@
 class StringCalculator
     def add(numbers)
-        raise "negatives not allowed - #{numbers}" if numbers.include?('-')
-        numbers.split("").map{|num| num.to_i}.sum
+        negatives = []
+        nums_arr = numbers.split("")
+        sum = 0
+        nums_arr.each_with_index do |num, idx|
+            if num == '-'
+                n = nums_arr[idx + 1].to_i
+                if n > 0
+                    negatives << -n
+                end
+            end
+            
+            int_num = num.to_i
+            if int_num < 0
+                negatives << int_num
+            else
+                sum += int_num
+            end
+        end
+        if negatives.empty?
+            sum
+        elsif negatives.length == 1
+            raise "negatives not allowed - #{negatives[0]}"
+        else
+            raise "negatives not allowed - #{negatives.join(", ")}"
+        end
     end
 end
